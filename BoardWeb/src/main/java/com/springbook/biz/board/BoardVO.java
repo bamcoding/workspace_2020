d99a466c3c949d65c5702af72381bef6b72d2889
gunj.lee <hanzen927@naver.com>
2020-01-22 00:51:50 +0900

    Spring Quick Start BoardWeb Project first Commit!

---
+++ BoardWeb/src/main/java/com/springbook/biz/board/BoardVO.java
@@ -0,0 +1,51 @@
+package com.springbook.biz.board;
+
+import java.sql.Date;
+
+public class BoardVO {
+
+	private int seq;
+	private String title;
+	private String write;
+	private String content;
+	private Date regDate;
+	private int cnt;
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWrite() {
+		return write;
+	}
+	public void setWrite(String write) {
+		this.write = write;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
+}
---
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -0,0 +1,17 @@
+package com.springbook.biz.board.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardDAO {
+
+	private Connection conn = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+	
+	private 
+}
